48bcf9f8e48f92146ac8535c24a378c0870ce6d3
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-08-01 13:54:12 +0100

    (fix): added proper annotation and changed obj name

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-"""Module which handles obfuscation"""
+"""Module which handles obfuscation
+"""
+
+import logging
 import re
 
 
@@ -7,9 +10,9 @@ def filter_datum(fields: str, redaction: str,
                  message: str, separator: str) -> str:
     """Obfuscate vital information in a string but splitting the string
     and filter through the string."""
-    message = message.split(separator)
+    messages: list[str] = message.split(separator)
     new_message: list = []
-    for mess in message:
+    for mess in messages:
         for field in fields:
             if re.search(field, mess):
                 mess = f"{field}=" + re.sub(f"^({field}=[a-z0-9//]*)*",
