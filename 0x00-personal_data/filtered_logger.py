aca629722d58a15735c3e8aa2a3397d56192ad50
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-08-01 21:53:42 +0100

    created new method for class to handle message formatting

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -15,7 +15,31 @@ def filter_datum(fields: str, redaction: str,
     for mess in messages:
         for field in fields:
             if re.search(field, mess):
-                mess = f"{field}=" + re.sub(f"^({field}=[a-z0-9//]*)*",
+                mess = f"{field}=" + re.sub(f"^({field}=[a-z0-9/@./-]*)*",
                                             redaction, mess)
         new_message.append(mess)
     return f"{separator}".join(new_message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: tuple[str]):
+        """class initializer
+        """
+        self.fields = list(fields)
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """converts the logging message into obfuscated data
+        which would be protected"""
+        messages = record.getMessage()
+        filter: str = filter_datum(self.fields, self.REDACTION,
+                                   messages, self.SEPARATOR)
+        record.msg = filter
+        return filter
