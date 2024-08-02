f9cd417e790d5afa4154286e7de5d5d630315ab7
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-08-02 23:17:26 +0100

    annotations

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@
 
 import logging
 import re
+from typing import List
 
 
 # def filter_datum(fields: str, redaction: str,
@@ -20,7 +21,7 @@ import re
 #         new_message.append(mess)
 #     return f"{separator}".join(new_message)
 
-def filter_datum(fields: list[str], redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Obfuscate vital information in a string but splitting the string
     and filter through the string."""
