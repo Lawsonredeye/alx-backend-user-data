b45ea5c498c42f26aef3c9ba6f690681f8931ab3
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-08-06 14:22:50 +0100

    (fix): implemented the basic_auth to filter through string to return the value to be encoded

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,14 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Inherits from the Auth
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Extracts base64 string or value to be encoded for http
+        """
+        if not isinstance(authorization_header, str):
+            return None
+        if authorization_header is None or authorization_header == "":
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.lstrip("Basic ")
