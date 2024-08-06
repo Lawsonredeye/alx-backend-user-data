9ee8e30e1d747a6365037bc45ce25c8dcf585863
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-08-06 15:11:53 +0100

    method which decodes a base64 string into a string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Basic auth module which handles and uses base64 for params
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,26 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header.lstrip("Basic ")
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        """Decode encoded base64 strings into strings
+        Returns:
+            Decoded base64 encoding into a string
+        Raises:
+            Exception when a string is being decoded instead of a byte-string
+        """
+        if base64_authorization_header is None:
+            return None
+        if base64_authorization_header == "":
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            base = base64.b64decode(
+                base64_authorization_header).decode("utf-8")
+            return base
+        except Exception as e:
+            return None
