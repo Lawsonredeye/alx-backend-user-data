6292bb2e47fae825b996953aa75e255d39a2c1b1
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-08-06 12:35:01 +0100

    (mod): change the logic to check the AUTH_TYPE value

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,7 +18,7 @@ auth = os.getenv("AUTH_TYPE")
 if auth == "basic_auth":
     from api.v1.auth.auth import BasicAuth
     auth = BasicAuth()
-else:
+elif auth != "basic_auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
