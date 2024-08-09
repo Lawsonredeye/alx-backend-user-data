066f1c3e1f3990b7d7e8d83931085ce681a1b94b
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-08-09 06:18:26 +0100

    created a session storing id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,22 @@
 """
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """"Childclass for Auth which handles new auth
     Mechanisim"""
-    pass
+    user_id_by_session_id: dict = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates session for new users or loggin
+        Returns:
+        - string session id
+        """
+        if user_id == None or not isinstance(user_id, str):
+            return None
+        else:
+            session_id: str = str(uuid.uuid4())
+            self.user_id_by_session_id[user_id] = session_id
+            return session_id
