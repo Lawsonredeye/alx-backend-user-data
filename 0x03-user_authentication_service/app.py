b3c007b63760c6b9129c04d3ecc74aae9f4d5aba
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-08-15 22:50:18 +0100

    added status code for post request

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -25,7 +25,7 @@ def users() -> Response:
     password: str = request.form.get("password")
     try:
         AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"})
+        return jsonify({"email": email, "message": "user created"}), 201
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
